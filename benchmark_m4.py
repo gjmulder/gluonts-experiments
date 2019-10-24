ebee17e24ca9e24903fd4c1b1a0d01a143ea5ed7
Gary Mulder <gjmulder@gmail.com>
2019-10-24 09:16:49 +0300

    Moved config into objective function for pickling. Looking at bigger DeepAREstimators, hyperopt smoketest on yokojitsu for m4_yearly

--- benchmark_m4.py
+++ benchmark_m4.py
@@ -6,14 +6,30 @@ Created on Tue Oct 22 11:09:36 2019
 @author: mulderg
 """
 
-import config
-logger = config.logger
+version = "0.1"
 
+from logging import basicConfig, getLogger
+#from logging import DEBUG as log_level
+from logging import INFO as log_level
+basicConfig(level = log_level,
+            format  = '%(asctime)s %(levelname)-8s %(module)-20s: %(message)s',
+            datefmt ='%Y-%m-%d %H:%M:%S')
+logger = getLogger(__name__)
+
+import mxnet as mx
+import numpy as np
 from functools import partial
 import sys
+#from math import log
 from datetime import date
-from numpy import random
-#import pandas as pd
+
+rand_seed = 42
+mx.random.seed(rand_seed, ctx='all')
+np.random.seed(rand_seed)
+
+use_cluster = False
+dataset_name = "m4_yearly"
+max_evals = 100
 
 ########################################################################################################
 
@@ -30,34 +46,38 @@ from hyperopt import fmin, tpe, hp, space_eval, STATUS_FAIL, STATUS_OK
 from hyperopt.mongoexp import MongoTrials
 
 ########################################################################################################
-
-def evaluate(dataset_name, estimator):
-    dataset = get_dataset(dataset_name)
-    estimator = estimator(
-        prediction_length=dataset.metadata.prediction_length,
-        freq=dataset.metadata.freq,
-    )
-
-    logger.info(f"Evaluating {estimator} on {dataset_name}")
-
-    predictor = estimator.train(dataset.train)
-
-    forecast_it, ts_it = make_evaluation_predictions(
-        dataset.test, predictor=predictor, num_eval_samples=100
-    )
-
-    agg_metrics, item_metrics = Evaluator()(
-        ts_it, forecast_it, num_series=len(dataset.test)
-    )
-
-#    pprint.pprint(agg_metrics)
-
-    eval_dict = agg_metrics
-    eval_dict["dataset"] = dataset_name
-    eval_dict["estimator"] = type(estimator).__name__
-    return eval_dict
     
 def gluon_fcast(cfg):
+
+    def evaluate(dataset_name, estimator):
+        dataset = get_dataset(dataset_name)
+        estimator = estimator(
+            prediction_length=dataset.metadata.prediction_length,
+            freq=dataset.metadata.freq,
+        )
+    
+        logger.info(f"Evaluating {estimator} on {dataset_name}")
+    
+        predictor = estimator.train(dataset.train)
+    
+        forecast_it, ts_it = make_evaluation_predictions(
+            dataset.test, predictor=predictor, num_eval_samples=100
+        )
+    
+        agg_metrics, item_metrics = Evaluator()(
+            ts_it, forecast_it, num_series=len(dataset.test)
+        )
+    
+    #    pprint.pprint(agg_metrics)
+    
+        eval_dict = agg_metrics
+        eval_dict["dataset"] = dataset_name
+        eval_dict["estimator"] = type(estimator).__name__
+        return eval_dict
+
+    if not use_cluster:
+        cfg['max_epochs'] = 2
+        
     logger.info("Params: %s" % cfg)
 #    use_feat_static_cat = True
 #        partial(
@@ -112,34 +132,39 @@ def gluon_fcast(cfg):
     )
     # catch exceptions that are happening during training to avoid failing the whole evaluation
     try:
-        results = evaluate(config.dataset_name, estimator)
+        results = evaluate(dataset_name, estimator)
     except Exception as e:
         logger.warn('Warning on line %d, exception: %s' % (sys.exc_info()[-1].tb_lineno, str(e)))
         return {'loss': None, 'status': STATUS_FAIL}
 
     logger.info(results)
     return {'loss': results['MASE'], 'status': STATUS_OK}
+
+# Daily: {'learning_rate_decay_factor': 0.575370116706172, 'max_epochs': 1000, 'num_batches_per_epoch': 100,
+#         'num_cells': 200, 'num_layers': 2, 'weight_decay': 2.432055488706233e-08}
     
 def call_hyperopt():
     space = {
-            'num_cells'                  : hp.choice('num_cells', [50, 100, 200]),
+            'num_cells'                  : hp.choice('num_cells', [200, 400, 800]),
             'num_layers'                 : hp.choice('num_layers', [1, 2, 3]),
 
-            'max_epochs'                 : hp.choice('max_epochs', [1000]),
-            'num_batches_per_epoch'      : hp.choice('num_batches_per_epoch', [25, 50, 100]),
-            'learning_rate_decay_factor' : hp.uniform('learning_rate_decay_factor', 0.5, 0.9),
-            'weight_decay'               : hp.loguniform('weight_decay', -19, -17),
+            'max_epochs'                 : hp.choice('max_epochs', [5000]),
+            'num_batches_per_epoch'      : hp.choice('num_batches_per_epoch', [100, 200, 400]),
+            'learning_rate_decay_factor' : hp.uniform('learning_rate_decay_factor', 0.5, 0.7),
+            'weight_decay'               : hp.loguniform('weight_decay', -18, -17),
         }
     
     # Search MongoDB for best trial for exp_key:
-    # echo 'db.jobs.find({"exp_key" : "XXXX-YYYY-MM-DD", "result.status" : "ok"}).sort( { "result.loss": 1} ).limit(1).pretty()' | mongo hyperopt_db
-    if config.use_cluster:
-        exp_key = "%s_%s" % (str(date.today()), config.version)
+    # echo 'db.jobs.find({"exp_key" : "XXX", "result.status" : "ok"}).sort( { "result.loss": 1} ).limit(1).pretty()' | mongo --host heika hyperopt_db
+    # echo 'db.jobs.remove({"exp_key" : "XXX", "result.status" : "new"})' | mongo --host heika hyperopt_db
+
+    if use_cluster:
+        exp_key = "%s_%s" % (str(date.today()), version)
         logger.info("exp_key for this job is: %s" % exp_key)
-        trials = MongoTrials('mongo://heika:27017/%s/jobs' % config.dataset_name, exp_key=exp_key)
-        best = fmin(gluon_fcast, space, rstate=random.RandomState(config.rand_seed), algo=tpe.suggest, trials=trials, max_evals=config.max_evals, show_progressbar=False)
+        trials = MongoTrials('mongo://heika:27017/%s/jobs' % dataset_name, exp_key=exp_key)
+        best = fmin(gluon_fcast, space, rstate=np.random.RandomState(rand_seed), algo=tpe.suggest, trials=trials, max_evals=max_evals, show_progressbar=False)
     else:
-        best = fmin(gluon_fcast, space, rstate=random.RandomState(config.rand_seed), algo=tpe.suggest, max_evals=20, show_progressbar=False)
+        best = fmin(gluon_fcast, space, rstate=np.random.RandomState(rand_seed), algo=tpe.suggest, max_evals=20, show_progressbar=False)
         
     params = space_eval(space, best)   
     return(params)
