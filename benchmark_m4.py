e8b671e72ad5350cf4ea5cffe436307a6f8c119d
Gary Mulder <gjmulder@gmail.com>
2019-10-30 12:34:35 +0200

    Cleaned up code. Added specific time features per data_set. Added script to check Mongo

--- benchmark_m4.py
+++ benchmark_m4.py
@@ -21,24 +21,22 @@ from hyperopt.mongoexp import MongoTrials
 from functools import partial
 from os import environ
 import sys
-#from math import log
+
+import mxnet as mx
 
 from gluonts.dataset.repository.datasets import get_dataset
-#from gluonts.distribution.piecewise_linear import PiecewiseLinearOutput
+from gluonts.time_feature import HourOfDay, DayOfWeek, DayOfMonth, DayOfYear, MonthOfYear
+#from gluonts.time_feature.holiday import SpecialDateFeatureSet, CHRISTMAS_DAY, CHRISTMAS_EVE
 from gluonts.evaluation import Evaluator
 from gluonts.evaluation.backtest import make_evaluation_predictions
-#from gluonts.model.deepar import DeepAREstimator
 from gluonts.model.deepstate import DeepStateEstimator
-#from gluonts.model.seq2seq import MQCNNEstimator
-#from gluonts.model.simple_feedforward import SimpleFeedForwardEstimator
 from gluonts.trainer import Trainer
-    
-########################################################################################################
-  
+
 rand_seed = 42
-import mxnet as mx
 mx.random.seed(rand_seed, ctx='all')
 np.random.seed(rand_seed)
+    
+########################################################################################################
 
 if ("DATASET" in environ) and ("VERSION" in environ):
     dataset_name = environ.get("DATASET")
@@ -47,43 +45,38 @@ if ("DATASET" in environ) and ("VERSION" in environ):
     version = environ.get("VERSION")
     logger.info("Using version : %s" % version)
     
-    job_url = environ.get("BUILD_URL")
     use_cluster = True
 else:
     dataset_name = "m4_daily"
     logger.warning("DATASET not set, using: %s" % dataset_name) 
 
     version = "test"
-    logger.warning("VERSION not set, using: %s" % version) 
+    logger.warning("VERSION not set, using: %s" % version)
     
-    job_url = "test"
     use_cluster = False
 
+if dataset_name == "m4_daily":
+    time_features = [DayOfWeek(), DayOfMonth(), MonthOfYear()]
+if dataset_name == "m4_hourly":
+    time_features = [HourOfDay(), DayOfWeek(), DayOfMonth(), DayOfYear(), MonthOfYear()]
 
+num_eval_samples = 1
+
+########################################################################################################
+    
 def gluon_fcast(cfg):   
     def evaluate(dataset_name, estimator):
         dataset = get_dataset(dataset_name)
-        estimator = estimator(
-            prediction_length=dataset.metadata.prediction_length,
-            freq=dataset.metadata.freq,
-        )
+        
+        estimator = estimator(prediction_length=dataset.metadata.prediction_length, freq=dataset.metadata.freq)
         estimator.ctx = mx.Context("cpu")
         
         logger.info(f"Evaluating {estimator} on {dataset_name}")
-    
         predictor = estimator.train(dataset.train)
         predictor.ctx = mx.Context("cpu")
         
-        forecast_it, ts_it = make_evaluation_predictions(
-            dataset.test, predictor=predictor, num_eval_samples=3
-        )
-    
-        agg_metrics, item_metrics = Evaluator()(
-            ts_it, forecast_it, num_series=len(dataset.test)
-        )
-    
-    #    pprint.pprint(agg_metrics)
-    
+        forecast_it, ts_it = make_evaluation_predictions(dataset.test, predictor=predictor, num_eval_samples=num_eval_samples)
+        agg_metrics, item_metrics = Evaluator()(ts_it, forecast_it, num_series=len(dataset.test))
         eval_dict = agg_metrics
         eval_dict["dataset"] = dataset_name
         eval_dict["estimator"] = type(estimator).__name__
@@ -92,47 +85,8 @@ def gluon_fcast(cfg):
     ##########################################################################
     
 #    if not use_cluster:
-#        cfg['max_epochs'] = 2
-        
+#        cfg['max_epochs'] = 2        
     logger.info("Params: %s" % cfg)
-#    use_feat_static_cat = True
-#        partial(
-#            DeepAREstimator,
-#            use_feat_static_cat=use_feat_static_cat,
-#            cardinality=[6],
-#            trainer=Trainer(
-#                epochs=epochs, num_batches_per_epoch=num_batches_per_epoch
-#            ),
-#        ),
-#        partial(
-#            DeepAREstimator,
-#            num_cells=500,
-#            num_layers=1,
-#            use_feat_static_cat=use_feat_static_cat,
-#            cardinality=[6],
-#            trainer=Trainer(
-#                epochs=epochs, num_batches_per_epoch=num_batches_per_epoch
-#            ),
-#        ),
-#        partial(
-#            DeepAREstimator,
-#            distr_output=PiecewiseLinearOutput(8),
-#            trainer=Trainer(
-#                epochs=epochs, num_batches_per_epoch=num_batches_per_epoch
-#            ),
-#        ),
-#        partial(
-#            MQCNNEstimator,
-#            use_feat_static_cat=use_feat_static_cat,
-#            cardinality=[6],
-#            trainer=Trainer(
-#                epochs=epochs, num_batches_per_epoch=num_batches_per_epoch
-#            ),
-#        ),
-    
-    
-    ########################################################################################################
-    # catch exceptions that are happening during training to avoid failing the whole evaluation
     try:    
         estimator = partial(
             DeepStateEstimator,
@@ -141,7 +95,9 @@ def gluon_fcast(cfg):
 #            dropout_rate=cfg['dropout_rate'],
             use_feat_static_cat=True,
             cardinality=[6],
-            num_eval_samples=3,
+            add_trend=cfg['add_trend'],
+            time_features=time_features, 
+            num_eval_samples=num_eval_samples,
             trainer=Trainer(
                 mx.Context("cpu"),
                 epochs=cfg['max_epochs'],
@@ -151,15 +107,14 @@ def gluon_fcast(cfg):
 #                learning_rate_decay_factor=cfg['learning_rate_decay_factor'],
 #                minimum_learning_rate=cfg['minimum_learning_rate'],
 #                weight_decay=cfg['weight_decay']
-            ),
-        )
+            ))
         results = evaluate(dataset_name, estimator)
     except Exception as e:
         logger.warning('Warning on line %d, exception: %s' % (sys.exc_info()[-1].tb_lineno, str(e)))
-        return {'loss': None, 'status': STATUS_FAIL, 'cfg' : cfg, 'job_url' : job_url}
+        return {'loss': None, 'status': STATUS_FAIL, 'cfg' : cfg, 'build_url' : environ.get("BUILD_URL")}
 
     logger.info(results)
-    return {'loss': results['MASE'], 'status': STATUS_OK, 'cfg' : cfg, 'job_url' : job_url}
+    return {'loss': results['MASE'], 'status': STATUS_OK, 'cfg' : cfg, 'build_url' : environ.get("BUILD_URL")}
 
 # Daily: DeepAREstimator
 #    cfg = {
@@ -176,18 +131,19 @@ def gluon_fcast(cfg):
 
 def call_hyperopt():
     space = {
-            'max_epochs'                 : hp.choice('max_epochs', [101]),
-            'num_batches_per_epoch'      : hp.choice('num_batches_per_epoch', [30, 40, 50, 60]),
-#            'batch_size'                 : hp.choice('batch_size', [32]),
+            'max_epochs'                 : hp.choice('max_epochs', [100]),
+            'num_batches_per_epoch'      : hp.choice('num_batches_per_epoch', [40, 50, 60, 70]),
+            'batch_size'                 : hp.choice('batch_size', [32, 64, 128]),
             
             'num_cells'                  : hp.choice('num_cells', [50, 100]),
-            'num_layers'                 : hp.choice('num_layers', [1, 2, 3, 4]),
+            'num_layers'                 : hp.choice('num_layers', [1, 2, 3]),
 
-#            'learning_rate'              : hp.uniform('learning_rate', 0.005, 0.010),
-#            'learning_rate_decay_factor' : hp.uniform('learning_rate_decay_factor', 0.3, 0.5),
-#            'minimum_learning_rate'      : hp.uniform('minimum_learning_rate', 1e-06, 1e-05),
-#            'weight_decay'               : hp.loguniform('weight_decay', -17.5, -16.7),
-#            'dropout_rate'               : hp.uniform('dropout_rate', 0.05, 0.15),
+            'add_trend'                  : hp.choice('add_trend', [True, False]),
+            'learning_rate'              : hp.uniform('learning_rate', 0.0005, 0.0015),
+            'learning_rate_decay_factor' : hp.uniform('learning_rate_decay_factor', 0.1, 0.9),
+            'minimum_learning_rate'      : hp.uniform('minimum_learning_rate', 01e-05, 10e-05),
+            'weight_decay'               : hp.uniform('weight_decay', 0.5e-08, 5.0e-08),
+            'dropout_rate'               : hp.uniform('dropout_rate', 0.05, 0.15),
         }
     
     # Search MongoDB for best trial for exp_key:
--- build_final.py
+++ build_final.py
@@ -6,8 +6,6 @@ Created on Tue Oct 22 11:09:36 2019
 @author: mulderg
 """
 
-version = "0.1"
-
 from logging import basicConfig, getLogger
 #from logging import DEBUG as log_level
 from logging import INFO as log_level
@@ -19,91 +17,81 @@ logger = getLogger(__name__)
 import numpy as np
 from functools import partial
 from os import environ
-import sys
 
-import pprint
+import mxnet as mx
 
 from gluonts.dataset.repository.datasets import get_dataset
+from gluonts.time_feature import HourOfDay, DayOfWeek, DayOfMonth, DayOfYear, MonthOfYear
 from gluonts.evaluation import Evaluator
 from gluonts.evaluation.backtest import make_evaluation_predictions
-from gluonts.model.deepar import DeepAREstimator
+from gluonts.model.deepstate import DeepStateEstimator
 from gluonts.trainer import Trainer
 
 rand_seed = 42
-import mxnet as mx
 mx.random.seed(rand_seed, ctx='all')
 np.random.seed(rand_seed)
+
+########################################################################################################
 
 if "DATASET" in environ:
     dataset_name = environ.get('DATASET')
     logger.info("Using data set: %s" % dataset_name)
     use_cluster = True
 else:
-    dataset_name = "m4_daily"        
+    dataset_name = "m4_hourly"        
     logger.warning("DATASET not set, using %s" % dataset_name)    
     use_cluster = False
 
+if dataset_name == "m4_daily":
+    time_features = [DayOfWeek(), DayOfMonth(), MonthOfYear()]
+if dataset_name == "m4_hourly":
+    time_features = [HourOfDay(), DayOfWeek(), DayOfMonth(), DayOfYear(), MonthOfYear()]
+
+num_eval_samples = 100
+
+########################################################################################################
+
 def evaluate(dataset_name, estimator):
     dataset = get_dataset(dataset_name)
-    estimator = estimator(
-        prediction_length=dataset.metadata.prediction_length,
-        freq=dataset.metadata.freq,
-    )
+    estimator = estimator(prediction_length=dataset.metadata.prediction_length, freq=dataset.metadata.freq)
+    estimator.ctx = mx.Context("cpu")
 
-    print(f"evaluating {estimator} on {dataset}")
+    logger.info(f"evaluating {estimator} on {dataset}")
 
     predictor = estimator.train(dataset.train)
+    predictor.ctx = mx.Context("cpu")
 
-    forecast_it, ts_it = make_evaluation_predictions(
-        dataset.test, predictor=predictor, num_eval_samples=100
-    )
-
-    agg_metrics, item_metrics = Evaluator()(
-        ts_it, forecast_it, num_series=len(dataset.test)
-    )
-
-    pprint.pprint(agg_metrics)
-
+    forecast_it, ts_it = make_evaluation_predictions(dataset.test, predictor=predictor, num_eval_samples=num_eval_samples)
+    agg_metrics, item_metrics = Evaluator()(ts_it, forecast_it, num_series=len(dataset.test))
+    logger.info(agg_metrics)
     eval_dict = agg_metrics
     eval_dict["dataset"] = dataset_name
     eval_dict["estimator"] = type(estimator).__name__
     return eval_dict
 
-
 if __name__ == "__main__":
     cfg = {
-			"dropout_rate" : 0.1275742856290955,
-			"learning_rate" : 0.008057677667662482,
-			"learning_rate_decay_factor" : 0.39481448324803753,
-			"max_epochs" : 5000,
-			"minimum_learning_rate" : 0.000009993770075059679,
-			"num_batches_per_epoch" : 60,
-			"num_cells" : 100,
-			"num_layers" : 2,
-			"weight_decay" : 4.560107496512775e-8
+        "max_epochs" : 100,
+        "num_batches_per_epoch" : 60,
+        "num_cells" : 100,
+        "num_layers" : 2,
+        "add_trend" : False
     }
-
     
-    ########################################################################################################
-    # catch exceptions that are happening during training to avoid failing the whole evaluation
-    try:    
-        estimator = partial(
-            DeepAREstimator,
-            num_cells=cfg['num_cells'],
-            num_layers=cfg['num_layers'],
-            dropout_rate=cfg['dropout_rate'],
-            use_feat_static_cat=True,
-            cardinality=[6],
-            trainer=Trainer(
-                epochs=cfg['max_epochs'],
-                num_batches_per_epoch=cfg['num_batches_per_epoch'],
-                learning_rate=cfg['learning_rate'],
-                learning_rate_decay_factor=cfg['learning_rate_decay_factor'],
-                minimum_learning_rate=cfg['minimum_learning_rate'],
-                weight_decay=cfg['weight_decay']
-            ),
+    estimator = partial(
+        DeepStateEstimator,
+        num_cells=cfg['num_cells'],
+        num_layers=cfg['num_layers'],
+        use_feat_static_cat=True,
+        cardinality=[6],
+        add_trend=cfg['add_trend'],
+        time_features=time_features, 
+        num_eval_samples=num_eval_samples,
+        trainer=Trainer(
+            mx.Context("cpu"),
+            epochs=cfg['max_epochs'],
+            num_batches_per_epoch=cfg['num_batches_per_epoch'])
         )
-        results = evaluate(dataset_name, estimator)
-        logger.info(results)
-    except Exception as e:
-        logger.warning('Warning on line %d, exception: %s' % (sys.exc_info()[-1].tb_lineno, str(e)))
+
+    results = evaluate(dataset_name, estimator)
+    logger.info(results)
